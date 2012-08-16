4695175c1cfce74c5fbaf607a4f3c4dde9c0f632
liwei <xiaowei942@sohu.com>
2012-08-16 15:35:57 +0800

    Complete EditText controls Activity's title application

--- controls/src/wei/apps/CtrlActivity.java
+++ controls/src/wei/apps/CtrlActivity.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.widget.Button; //引入类
 import android.view.View;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.text.Editable;
+import android.text.TextWatcher;
 import java.util.Date;
 
 public class CtrlActivity extends Activity implements View.OnClickListener
 {
 	Button btn_show_time; //声明对象
 	TextView text_view_1;
+	EditText edit_text_1;
+	
 	
     /** Called when the activity is first created. */
     @Override
@@ -23,6 +28,28 @@ public class CtrlActivity extends Activity implements View.OnClickListener
         btn_show_time.setOnClickListener(this);                      //设置监听器
         
         text_view_1 = (TextView)findViewById(R.id.textview1);
+        edit_text_1 = (EditText)findViewById(R.id.edittext1);
+        
+        edit_text_1.addTextChangedListener(new TextWatcher(){
+
+			public void afterTextChanged(Editable arg0) {
+				// TODO Auto-generated method stub
+				setTitle(edit_text_1.getText());
+			}
+
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+        	
+        });
     }
     
     //实现响应函数
